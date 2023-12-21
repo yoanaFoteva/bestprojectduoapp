fbc0bc66d9bb9fab02f7214b0c21e3def52fa145
yoanafoteva <rm_619@abv.bg>
2023-12-21 13:48:32 +0200

    update csv reader

--- src/main/java/com/academy/bestprojectduoapp/util/CustomReader.java
+++ src/main/java/com/academy/bestprojectduoapp/util/CustomReader.java
@@ -1,8 +1,9 @@
 package com.academy.bestprojectduoapp.util;
 
 import com.academy.bestprojectduoapp.model.WorkHistory;
-import java.util.List;
 
-interface CustomReader {
-    public List<WorkHistory> read (String filename);
+import java.io.IOException;
+import java.util.List;
+public interface CustomReader {
+    List<WorkHistory> read(String fileName) throws IOException;
 }
