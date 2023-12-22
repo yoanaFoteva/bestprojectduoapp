98bf386d145daf9870b8cda88c8efefb12b64270
yoanafoteva <rm_619@abv.bg>
2023-12-22 13:09:41 +0200

    delete unnecessary code

--- src/main/java/com/academy/bestprojectduoapp/repository/WorkHistoryRepository.java
+++ src/main/java/com/academy/bestprojectduoapp/repository/WorkHistoryRepository.java
@@ -4,12 +4,7 @@ import com.academy.bestprojectduoapp.model.WorkHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface WorkHistoryRepository extends JpaRepository <WorkHistory, Long> {
-    Optional<List<WorkHistory>> findByEmployeeID (Long employeeID);
 
-    Optional<List<WorkHistory>> findByProjectID (Long projectID);
 }
